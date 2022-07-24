db9f84509977071aba88f691a7d2c06efe938d4b
filiptubic <tubefilip@gmail.com>
2022-07-24 12:38:27 +0200

    add bluetooth notif about connected device

--- .config/polybar/bluetooth/bluetooth.sh
+++ .config/polybar/bluetooth/bluetooth.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
-  echo ""
+  echo ""
 else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then 
-    echo ""
+  connected=$(bluetoothctl devices | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep -e "Name\|Connected: yes" | grep -B1 "yes" | head -n 1 | cut -d\  -f2-)
+  
+  if [ -z "${connected}" ];
+  then
+    echo ""
   fi
-  echo ""
+  echo " %{T1}$connected%{T-}"
 fi
