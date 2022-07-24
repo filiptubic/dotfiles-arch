df7377c19045a664f14af2dca2453484ba8f336c
filiptubic <tubefilip@gmail.com>
2022-07-24 17:44:14 +0200

    add custom py script for connected bluetooth devices

--- .config/polybar/bluetooth/bluetooth.sh
+++ .config/polybar/bluetooth/bluetooth.sh
@@ -3,8 +3,7 @@ if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
   echo ""
 else
-  connected=$(bluetoothctl devices | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep -e "Name\|Connected: yes" | grep -B1 "yes" | head -n 1 | cut -d\  -f2-)
-  
+  connected=$(python $HOME/.config/polybar/bluetooth/script.py)
   if [ -z "${connected}" ];
   then
     echo ""
---
+++ .config/polybar/bluetooth/script.py
@@ -0,0 +1,51 @@
+import subprocess
+
+cmd = r'''bluetoothctl paired-devices | cut -f2 -d' '|
+while read -r uuid
+do
+    info=`bluetoothctl info $uuid`
+    if echo "$info" | grep -q "Connected: yes"; then
+       echo "$info" | grep "Name"
+    fi
+done
+'''
+
+def extract_uuid(device):
+    return device.lstrip('Device').strip().split(' ')[0].strip()
+
+def extract_name(device):
+    return ' '.join(device.strip().split(' ')[2:])
+
+def list_paired():
+    process = subprocess.Popen(['bluetoothctl', 'paired-devices'], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    
+    if error:
+        exit(1)
+
+    return output.decode("utf-8").strip()
+
+
+def is_connected(uuid):
+    process = subprocess.Popen(['bluetoothctl', 'info', uuid], stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+    if error:
+        exit(1)
+
+    return 'Connected: yes' in output.decode("utf-8").strip()
+  
+
+if __name__ == '__main__':
+    paired_devices = list_paired()
+    
+    connected = []
+    for line in paired_devices.split('\n'):
+        device_uuid = extract_uuid(line)
+
+        if is_connected(device_uuid):
+            name = extract_name(line)
+            connected.append(name)
+
+    print(' | '.join(connected))
+    
